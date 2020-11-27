50b441896d373545f6201b1f67874eda121bea4b
Yancey Leo <yanceyofficial@gmail.com>
2020-11-27 14:16:26 +0800

    feat: update types of typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
   },
   overrides: [
     {
